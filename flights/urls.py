48a6b90ee9ec4f9ce6b23282261b63c11bcfe19b
chirag982 <chirag178g@gmail.com>
2024-08-31 00:08:57 +0530

    Here basically i updated flights app in that i created model User updated some of the css and made admin working page and created a superuser

--- flights/urls.py
+++ flights/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    
-    path("login/", views.login, name="login"),
-    path("signup/", views.signup, name="signup")
+    path("", views.login, name="login"),
+    path("signup/", views.signup, name="signup"),
+    path("index/", views.index, name="index")
 ]
